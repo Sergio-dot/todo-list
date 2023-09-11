81e997b69ebb1b0aa178e04600aacf79b2a763c7
Sergio <sergio.siclari@gmail.com>
2023-09-11 17:37:39 +0200

    integrated MariaDB

---
+++ modules/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mdb = require('mariadb');
+
+const pool = mdb.createPool({
+  host: 'localhost',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+module.exports = {
+  getConnection: function () {
+    return new Promise(function (resolve, reject) {
+      pool
+        .getConnection()
+        .then(function (connection) {
+          resolve(connection);
+        })
+        .catch(function (error) {
+          reject(error);
+        });
+    });
+  },
+};
--- src/app.js
+++ src/app.js
@@ -1,14 +1,42 @@
 require('dotenv').config();
 const express = require('express');
+const pool = require('../modules/db');
 
+const port = process.env.PORT || 4001;
+
+// Init express
 const app = express();
 
+// Middlewares
 app.use(express.json());
 
+// Routes
 app.get('/', (req, res) => {
   res.send('Todo App Project');
 });
 
-app.listen(process.env.PORT, () => {
+app.get('/users', async (req, res) => {
+  let conn;
+  try {
+    // connect to database
+    conn = await pool.getConnection();
+
+    // query
+    var query = 'select * from users';
+
+    // execute the query
+    var rows = await conn.query(query);
+
+    // show results
+    res.send(rows);
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.release();
+  }
+});
+
+// Start web server
+app.listen(port, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
