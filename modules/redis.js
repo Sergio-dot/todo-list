1a06c9bc9a33e03164390cd700f30c47248966e1
Sergio <sergio.siclari@gmail.com>
2023-09-12 15:57:12 +0200

    fixed redis connecting to localhost

--- modules/redis.js
+++ modules/redis.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const redis = require('redis');
 
-console.log(process.env.REDIS_URL);
-
 const client = redis.createClient({
   url: process.env.REDIS_URL || 'redis://redis:6379',
 });
