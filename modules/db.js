940b6a2d87ea158acd67a7a2a6ef028a9e26eb30
Sergio <sergio.siclari@gmail.com>
2023-09-12 10:36:35 +0200

    fixed database connection timeout

--- modules/db.js
+++ modules/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mdb = require('mariadb');
 
 const pool = mdb.createPool({
-  host: 'localhost',
+  host: 'db',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
