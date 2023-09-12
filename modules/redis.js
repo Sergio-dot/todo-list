c336e603be4fa3b9d316c9b86cd3ab2d30effef4
Sergio <sergio.siclari@gmail.com>
2023-09-12 23:15:24 +0200

    refactored

--- modules/redis.js
+++ modules/redis.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const redis = require('redis');
 
 const client = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://redis:6379',
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
 });
 
 module.exports = client;
