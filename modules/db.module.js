c20b09bdd31e113a4591a26997bc8fbcaf862ef7
Sergio <sergio.siclari@gmail.com>
2023-09-19 14:34:34 +0200

    updated database connection info

--- modules/db.module.js
+++ modules/db.module.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mdb = require('mariadb');
 
 const pool = mdb.createPool({
-  host: process.env.MYSQL_HOST || '',
+  // host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
