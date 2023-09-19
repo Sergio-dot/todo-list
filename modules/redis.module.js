abe22da02d468a6302c5b6c5c5397897f72bd3f8
Sergio <sergio.siclari@gmail.com>
2023-09-19 14:44:25 +0200

    updated redis connection

--- modules/redis.module.js
+++ modules/redis.module.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const redis = require('redis');
 
 const client = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379',
+  url: process.env.REDIS_URL,
 });
 
 console.log(' [-] Connecting to Redis...');
